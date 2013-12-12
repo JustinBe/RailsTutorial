b6b2a4512bfd679d5a888e5e0588208ac10924c4
Bennett, Justin <justin.bennett@connextions.com>
2013-12-12 14:31:09 -0500

    Redirect users attempting to access projects that do not exist.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   include ProjectsHelper
+
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
   
   def index
     @projects = Project.all
@@ -15,23 +17,52 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    get_project_with_id params[:id]
   end
 
   def edit
-    get_project_with_id params[:id]
   end
 
   def update
-    get_project_with_id params[:id]
-
     validate_edit_project_and_save params[:project]
   end
 
   def destroy
-    get_project_with_id params[:id]
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
+
+private
+  def get_project_with_id id
+    @project = Project.find id
+  end
+
+  def find_project
+    get_project_with_id params[:id]
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking" +
+    " for could not be found."
+    redirect_to projects_path
+  end
+
+  def validate_new_project_and_save
+    if @project.save
+      flash[:notice] = 'Project has been created.'
+      redirect_to @project
+    else
+      flash[:alert] = 'Project has not been created.'
+      render :action => 'new'
+    end
+  end
+
+  def validate_edit_project_and_save project_to_edit
+    if @project.update_attributes project_to_edit
+      flash[:notice] = 'Project has been updated.'
+      redirect_to @project
+    else
+      flash[:alert] = 'Project has not been updated.'
+      render :action => 'edit'
+    end
+  end
+
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,27 +1,3 @@
 module ProjectsHelper
 
-  def get_project_with_id id
-    @project = Project.find id
-  end
-
-  def validate_new_project_and_save
-    if @project.save
-      flash[:notice] = 'Project has been created.'
-      redirect_to @project
-    else
-      flash[:alert] = 'Project has not been created.'
-      render :action => 'new'
-    end
-  end
-
-  def validate_edit_project_and_save project_to_edit
-    if @project.update_attributes project_to_edit
-      flash[:notice] = 'Project has been updated.'
-      redirect_to @project
-    else
-      flash[:alert] = 'Project has not been updated.'
-      render :action => 'edit'
-    end
-  end
-
 end
