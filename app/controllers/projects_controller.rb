e84a947b6abfe0bd092aed3e51da09821994fdfb
Bennett, Justin <justin.bennett@connextions.com>
2013-12-12 13:55:12 -0500

    Added base functionality for editing projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,10 +15,21 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find params[:id]
+    get_project_with_id params[:id]
+  end
+
+  def edit
+    get_project_with_id params[:id]
   end
 
   def update
-    
+    get_project_with_id params[:id]
+    if @project.update_attributes params[:project]
+      flash[:notice] = 'Project has been updated.'
+      redirect_to @project
+    else
+      flash[:alert] = 'Project has not been updated.'
+      render :action => 'edit'
+    end
   end
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,7 @@
 module ProjectsHelper
+  def get_project_with_id id
+    @project = Project.find id
+  end
   def validate_new_project_and_save
     if @project.save
       flash[:notice] = 'Project has been created.'
